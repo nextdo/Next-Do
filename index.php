d87aa15c8a3fbf641dc8c8999f7251be5b102a1a
John Haugeland <stonecypher+nextdo@gmail.com>
2010-01-03 00:42:36 -0700

    Updates to index, moved initializer and simple_renderer, added test rig

--- index.php
+++ index.php
@@ -4,14 +4,12 @@
 
 
 
-  require_once('infrastructure/nxd_notice.php');
-
   require_once('config/nxd_config.php');
-
   if ($GLOBALS['nxd_config']['disable_this_install']) { die('This installation has been disabled.'); }
 
-  require_once('nxd_simple_renderer.php');
-  require_once('nxd_initializer.php');
+  require_once('skins/nxd_simple_renderer.php');
+  require_once('infrastructure/nxd_notice.php');
+  require_once('infrastructure/nxd_initializer.php');
 
 
 
